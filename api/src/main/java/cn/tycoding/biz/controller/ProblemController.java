cad49732805f70ff3f348661b22cf57ad7502861
Q22 <yiruqiin@outlook.com>
2023-06-22 15:42:08 +0800

    更新展示图片

--- api/src/main/java/cn/tycoding/biz/controller/ProblemController.java
+++ api/src/main/java/cn/tycoding/biz/controller/ProblemController.java
@@ -15,7 +15,6 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping(CommonConstant.BASE_API + "/problem")
